da764cb10ac4ff803d475924bcb96c5e530b59e2
FatChicken277 <aramirez@truora.com>
2021-10-14 15:55:53 -0500

    add mongo utils

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, {
+      useUnifiedTopology: true,
+    });
+
+    this.client.connect();
+    this.db = this.client.db(this.database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = await this.db.collection('users');
+
+    return collection.countDocuments();
+  }
+
+  async nbFiles() {
+    const collection = await this.db.collection('files');
+
+    return collection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
