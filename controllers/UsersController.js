233e05676fa6de616668297a33200a2d91f15b69
FatChicken277 <aramirez@truora.com>
2021-10-14 17:57:04 -0500

    add usersCOntroller

---
+++ controllers/UsersController.js
@@ -0,0 +1,45 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+function toHashPassword(password) {
+  return crypto
+    .createHash('sha1')
+    .update(JSON.stringify(password))
+    .digest('hex');
+}
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    const getUsersQuery = await dbClient.db
+      .collection('users')
+      .find({ email })
+      .toArray();
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    if (getUsersQuery.length > 0) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const addUserQuery = await dbClient.db
+      .collection('users')
+      .insertOne({ email, password: toHashPassword(password) });
+
+    const userToResponse = {
+      id: addUserQuery.ops[0]._id,
+      email: addUserQuery.ops[0].email,
+    };
+
+    return res.status(201).json(userToResponse);
+  }
+}
+
+module.exports = UsersController;
